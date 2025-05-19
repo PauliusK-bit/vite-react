f26d234bb94f8bfc1a4a30fd2bccf9c0d3d036c3
PauliusK-bit <kareiva.paulius13@gmail.com>
2025-05-19 18:09:36 +0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <Navigation />
+      <div>
+        <img
+          src="/images/booker/white/Ekrano kopija 2025-05-16 140715.png"
+          alt=""
+        />
+      </div>
     </>
   );
 }
